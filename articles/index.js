06d0d274983c13c947bafeec3b5c42ff842290da
vikoss <vikoss.yiss@gmail.com>
2022-02-03 16:57:43 -0600

    add static array to render the articles

---
+++ articles/index.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    name: 'crear-bot-telegram',
+    title: 'Como crear un bot y enviar mensajes a un grupo con Telegram',
+    description:
+      'Necesitas crear un bot... pues adelante que vamos a crear uno y ademas veremos como enviar mensajes a un grupo con una simple solicitud HTTP utilizando Telegram y Laravel.',
+    image: require('~/assets/blogs/crear-bot-telegram/telegram.jpg'),
+  },
+]
